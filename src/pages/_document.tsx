f906724dadcebf2cbdf78a6a99922711440502bf
Jakov Knezevic <knezevic.jakov@gmail.com>
2022-10-29 09:57:22 +0200

    Bugfix. Removed zod package.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,29 +4,20 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link
-          rel="preconnect"
-          href="https://fonts.googleapis.com"
-          height={"auto"}
-          width={"auto"}
-        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin="true"
-          height={"auto"}
-          width={"auto"}
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Plus+Jakarta+Sans:wght@400;500;700;800&display=swap"
           rel="stylesheet"
-          height={"auto"}
-          width={"auto"}
         />
       </Head>
       <body>
-        <Main height={"auto"} width={"auto"} />
-        <NextScript height={"auto"} width={"auto"} />
+        <Main />
+        <NextScript />
       </body>
     </Html>
   );
