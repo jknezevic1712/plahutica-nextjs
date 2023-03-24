a86541aa0b7e499e0389c477433bab034ac79e40
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-03-24 20:08:54 +0100

    Added @vercel/analytics package to track users interactions on the blog.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+// Analytics
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
 
@@ -8,6 +11,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <AppWrapper>
       <Component {...pageProps} height={"auto"} width={"auto"} />
+      <Analytics />
     </AppWrapper>
   );
 };
