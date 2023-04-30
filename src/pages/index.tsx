e0d89bf803786200ab6ed9d9abc11178fae52be2
Jakov Knezevic <knezevic.jakov@gmail.com>
2023-04-30 22:22:19 +0200

    Changed app title and meta keywords. Added new search engine background image.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,11 +17,11 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>Plahutica blog</title>
-        <meta title="Plahutica blog"></meta>
+        <meta title="Budget travel blog"></meta>
         <meta charSet="UTF-8" />
         <meta
           name="keywords"
-          content="jednodnevni izlet iz Zagreba, Zagorje, Istra, što posjetiti u Istri, Pula, Rim putovanje, Prag putovanje"
+          content="jednodnevni izlet iz Zagreba, Zagorje, Istra, što posjetiti u Istri, Pula, Rim putovanje, Prag putovanje, budget travel blog, budget, travel, blog"
         />
         <meta name="description" content="Savjeti za budget putovanja!" />
         <meta name="author" content="Jakov Knežević & Ema Belošević" />
@@ -33,20 +33,20 @@ const Home: NextPage = () => {
         <meta name="robots" content="index, follow" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Plahutica blog" />
+        <meta property="og:title" content="Budget travel blog" />
         <meta
           property="og:description"
           content="Savjeti za budget putovanja!"
         />
-        <meta property="og:image" content="/background.jpg" />
+        <meta property="og:image" content="/search-engine-bg.webp" />
         <meta property="og:url" content="https://plahutica.vercel.app/" />
-        <meta property="og:site_name" content="Plahutica blog" />
-        <meta name="twitter:title" content="Plahutica blog" />
+        <meta property="og:site_name" content="Budget travel blog" />
+        <meta name="twitter:title" content="Budget travel blog" />
         <meta
           name="twitter:description"
           content="Savjeti za budget putovanja!"
         />
-        <meta name="twitter:image" content="/background.jpg" />
+        <meta name="twitter:image" content="/search-engine-bg.webp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
